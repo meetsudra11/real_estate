94d2f6f802b98ba93e94c627661aa038bc458fd9
meetsudra_ <meetsudra03@gmail.com>
2025-07-27 15:41:59 +0530

    Add files via upload

---
+++ Interface_files/Home.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+st.set_page_config(
+    page_title = "Gurgaon Real Estate App",
+    page_icon = "👋🏻",
+)
+
+st.write('# Welcome to Streamlit!👋🏻')
+st.sidebar.success('Select a demo above.')
